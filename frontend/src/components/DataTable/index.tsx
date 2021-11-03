905dc9023c3ffedbf07c0c0003df7d15c6b3af08
Anderson Castro <andersonkstro@mail.com>
2021-11-03 19:51:20 -0300

    values datatable

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -24,63 +24,63 @@ const DataTable = () => {
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>13345.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>16751.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>12763.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>15567.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>18009.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>15505.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>16666.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>18756.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
-                        <td>15017.00</td>
+                        <td>14555.00</td>
                     </tr>
                 </tbody>
             </table>
